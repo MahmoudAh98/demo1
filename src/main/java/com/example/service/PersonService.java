1284320c62e1d58979721f982a8322c7b40c1623
MahmoudAh98 <92112305+MahmoudAh98@users.noreply.github.com>
2025-08-27 19:14:04 +0300

    add mahmoud for test purposes

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,6 +39,13 @@ public class PersonService {
 		p.setFirstName("AbdulHalim");
 		p.setLastName("Mahmoud");
 		persons.add(p);
+
+		p = new Person();
+		p.setId("5");
+		p.setAge(26);
+		p.setFirstName("Mahmoud");
+		p.setLastName("Ahmed");
+		persons.add(p);
 		
 	}
 	public Person getPerson(String id) {
