3895cb62a9e300fcddcbb08183331dbe79107868
MahmoudAh98 <92112305+MahmoudAh98@users.noreply.github.com>
2025-08-29 13:14:28 +0300

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -46,6 +46,13 @@ public class PersonService {
 		p.setFirstName("Mahmoud");
 		p.setLastName("Ahmed");
 		persons.add(p);
+
+		p = new Person();
+		p.setId("6");
+		p.setAge(25);
+		p.setFirstName("Mohammed");
+		p.setLastName("Nasser");
+		persons.add(p);
 		
 	}
 	public Person getPerson(String id) {
